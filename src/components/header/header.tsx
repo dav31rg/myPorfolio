d8d4f6ad3435811e7ab67205de4a7f8729812eb8
davel <davel.reymundo@gmail.com>
2025-08-07 17:36:41 -0500

    feat(header): add header component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,46 +1,96 @@
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import hamburger from "../../assets/hamburger.svg";
 import styles from "./header.module.css";
+import { Link } from "react-router-dom";
 
-const Header: React.FC = () => {
-  const [isActive, setIsActive] = useState("hero");
+interface HeaderProps {
+  info: string[];
+}
 
-  const handleClick = (sectionId: string) => {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      const yOffset = -255; // Ajusta este valor según el margen deseado
-      const yPosition =
-        section.getBoundingClientRect().top + window.scrollY + yOffset;
+const Header: React.FC<HeaderProps> = ({ info }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef<HTMLUListElement | null>(null);
 
-      window.scrollTo({ top: yPosition, behavior: "smooth" });
-    }
-    setIsActive(sectionId);
+  const handelMenuView = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
   };
 
+  const getRouterPath = (item: string) => (item === "home" ? "/" : `/${item}`);
+
+  useEffect(() => {
+    if (!isMenuOpen) return;
+
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsMenuOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isMenuOpen]);
+
   return (
-    <section className={styles.header}>
+    <section className={styles.light}>
+      {/* Desktop View */}
       <ul className={styles.list}>
-        <motion.li
-          whileHover={{ scale: 1.3 }}
-          transition={{type: "spring", damping: 5, stiffness: 100}}
-          className={isActive === "hero" ? styles.active : ""}
-          onClick={() => handleClick("hero")}
-        >
-          Home
-        </motion.li>
-        <motion.li
-          whileHover={{ scale: 1.3 }}
-          transition={{type: "spring", damping: 5, stiffness: 100}}
-          className={isActive === "about" ? styles.active : ""}
-          onClick={() => handleClick("about")}
-        >
-          About
-        </motion.li>
-        <li>Skills</li>
-        <li>Experience</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
+        {info.map((item, index) => {
+          return (
+            <motion.li
+              whileHover={{ scale: 1.3 }}
+              transition={{ type: "spring", damping: 5, stiffness: 100 }}
+              key={index}
+            >
+              <Link to={getRouterPath(item)} className={styles.navLink}>
+                {item}
+              </Link>
+            </motion.li>
+          );
+        })}
       </ul>
+
+      {/* Mobile View */}
+      <div
+        onClick={handelMenuView}
+        className={styles.mobile}
+        role="button"
+        aria-label="Toggle menu"
+      >
+        <img src={hamburger} alt="hamburguer" width={24} height={24} />
+      </div>
+      <AnimatePresence>
+        {isMenuOpen && (
+          <motion.ul
+            className={styles.menuMobile}
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }}
+            transition={{ duration: 0.3 }}
+            ref={menuRef}
+          >
+            {info.map((item, index) => (
+              <motion.li
+                whileHover={{ scale: 1.3 }}
+                transition={{ type: "spring", damping: 5, stiffness: 100 }}
+                key={index}
+              >
+                <Link
+                  to={getRouterPath(item)}
+                  onClick={handleCloseMenu}
+                  className={styles.navLink}
+                >
+                  {item}
+                </Link>
+              </motion.li>
+            ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
     </section>
   );
 };
