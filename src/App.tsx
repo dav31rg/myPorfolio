3fadecc14f5f2a7599aa6266831a2bff26157332
davel <davel.reymundo@gmail.com>
2025-08-07 17:42:25 -0500

    feat(App): add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-
+import About from "./components/about/about";
+import Experience from "./components/experience/experience";
+import Footer from "./components/footer/footer";
+import Header from "./components/header/header";
+import Hero from "./components/hero/hero";
+import NotFound from "./components/notFound/notFound";
+import Portfolio from "./components/portfolio/portfolio";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { info } from "./utils/info";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-    <h1>Hi there</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Header info={info.header} />
+      <Routes>
+        <Route path="/" element={<Hero info={info.personal} />} />
+        <Route path="/about" element={<About info={info.about} />} />
+        <Route
+          path="/experience"
+          element={<Experience info={info.experience} />}
+        />
+        <Route path="/projects" element={<Portfolio info={info.projects} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
